117797b4ce361dc9dfcde85785eb3f0c53c05865
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-05 19:42:27 -0300

    test(src/index.js): se agrega un test de ejemplo

---
+++ generators/app/templates/common_structure/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import { faker } from './helpers'
+import { getGreeting } from '../src/index'
+
+const filePath = 'src/index.js'
+
+describe(`export function getGreeting (${filePath})`, () => {
+  it('Should return a string that includes the hello word', () => {
+    // Arrange
+    const string = faker.string.sample()
+
+    // Act
+    const result = getGreeting(string)
+
+    // Assert
+    expect(result).toMatch(/hello /i)
+  })
+})
