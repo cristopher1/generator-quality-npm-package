7288f0cab8eea3af1a21086670c174a80d7b6a00
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-05 22:26:14 -0300

    fix(index.js): se agregan valores por defecto a prompt

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,10 +18,10 @@ export default class extends Generator {
     this.fs.copy(this.templatePath(`${src}/.**/*`), this.destinationPath(dst))
   }
 
-  #copyNormalTemplates(src, templateData, dts = '') {
+  #copyNormalTemplates(src, templateData, dst = '') {
     this.fs.copyTpl(
       this.templatePath(`${src}/**/*`),
-      this.destinationPath(dts),
+      this.destinationPath(dst),
       templateData,
     )
   }
@@ -174,6 +174,7 @@ export default class extends Generator {
             value: 'module',
           },
         ],
+        default: 'commonjs',
       },
       {
         type: 'list',
@@ -190,6 +191,7 @@ export default class extends Generator {
             value: false,
           },
         ],
+        default: true,
       },
     ]
 
@@ -207,41 +209,7 @@ export default class extends Generator {
     this.#copyNormalTemplates(templateFiles, this.answers)
   }
 
-  install() {
-    this.addDependencies({
-      '@babel/runtime-corejs3': '^7.22.11',
-      'core-js': '^3.32.1',
-    })
-    this.addDevDependencies({
-      '@babel/cli': '^7.22.10',
-      '@babel/core': '^7.22.10',
-      '@babel/plugin-transform-runtime': '^7.22.10',
-      '@babel/preset-env': '^7.22.10',
-      '@commitlint/cli': '^17.8.0',
-      '@commitlint/config-conventional': '^17.8.0',
-      '@faker-js/faker': '^8.0.2',
-      '@rollup/plugin-babel': '^6.0.3',
-      '@rollup/plugin-node-resolve': '^15.2.0',
-      eslint: '^8.47.0',
-      'eslint-config-prettier': '^9.0.0',
-      'eslint-config-standard': '^17.1.0',
-      'eslint-plugin-import': '^2.28.1',
-      'eslint-plugin-jest': '^27.2.3',
-      'eslint-plugin-jsdoc': '^46.8.2',
-      'eslint-plugin-n': '^16.0.2',
-      'eslint-plugin-promise': '^6.1.1',
-      husky: '^8.0.3',
-      jest: '^29.7.0',
-      'lint-staged': '^14.0.1',
-      prettier: '^3.0.2',
-      'prettier-plugin-jsdoc': '^1.1.1',
-      'readme-md-generator': '^1.0.0',
-      rimraf: '^5.0.1',
-      rollup: '^3.28.0',
-      'rollup-plugin-dts': '^6.1.0',
-      typescript: '^5.2.2',
-    })
-  }
+  install() {}
 
   end() {
     const { runCommands } = this.answers
