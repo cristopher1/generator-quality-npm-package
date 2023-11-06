dd412451c588ade4c30a6ffb77fa93ca94302e89
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-06 16:04:23 -0300

    build: se modifica la configuración de rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { getBabelOutputPlugin } from '@rollup/plugin-babel'
+import { dts } from 'rollup-plugin-dts'
 
 const getOutputFile = (basePath, { outputDir, name, extension }) => {
   return path.resolve(basePath, outputDir, `${name}.${extension}`)
@@ -15,13 +16,23 @@ const CJS_FILE = {
   outputDir: 'cjs',
   babelEnvName: 'buildCommonJS',
 }
+const DTS_FILE = {
+  extension: 'd.ts',
+  format: 'es',
+  name: 'index',
+  outputDir: 'types',
+}
 
 // input files
-const INPUT_SRC_FILE = 'src/index.js'
+const INPUT_SRC_FILE = 'generators/app/index.js'
+const INPUT_DTS_FILE = 'dist/tmp/types/index.d.ts'
 
 // transpiled files
 const BASE_DIR = 'dist'
 const OUTPUT_CJS_FILE = getOutputFile(BASE_DIR, CJS_FILE)
+
+// type declaration file
+const OUTPUT_DTS_FILE = getOutputFile(BASE_DIR, DTS_FILE)
 
 // getBabelOutputPlugin configuration
 const BABEL_CONFIG_FILE = path.resolve('.', 'babel.config.json')
@@ -44,5 +55,15 @@ export default defineConfig([
     ],
     external: [/node_module/],
     plugins: [nodeResolve()],
+  },
+  {
+    input: INPUT_DTS_FILE,
+    output: [
+      {
+        file: OUTPUT_DTS_FILE,
+        format: DTS_FILE.format,
+      },
+    ],
+    plugins: [dts()],
   },
 ])
