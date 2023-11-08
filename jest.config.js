cd30a83141f64659e6d1a3fbeacad44b1435c988
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-11-08 00:10:59 -0300

    build: se ignoran los tests encontrados en la carpeta dist

--- jest.config.js
+++ jest.config.js
@@ -90,7 +90,7 @@ const config = {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  modulePathIgnorePatterns: ['helpers', 'generators'],
+  modulePathIgnorePatterns: ['helpers', 'generators', 'dist'],
 
   // Activates notifications for test results
   // notify: false,
